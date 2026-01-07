b7971ad2b45917e2c58d1c19178c55ee08789465
Martin <me@martinosorio.com>
2026-01-07 17:24:25 -0500

    Fix versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.martinosorio.dayssince"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.martinosorio.dayssince"
