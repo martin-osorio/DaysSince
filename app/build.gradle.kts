f9f8b1903470b6ae22adde589b5e1c1a8d3a1772
Martin <me@martinosorio.com>
2026-02-03 12:23:59 -0500

    Prepare for initial release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.martinosorio.dayssince"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
